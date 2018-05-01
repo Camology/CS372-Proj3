7d1768b67b1a8d74ab797db3867dbf967f2b9ca0
William Horn <wbhorn@alaska.edu>
2018-04-30 22:29:29 -0800

    Refactor controller select 'Start' method into multiple functions.

--- Battle Pong/Assets/Scripts/ControllerSelect.cs
+++ Battle Pong/Assets/Scripts/ControllerSelect.cs
@@ -1,27 +1,40 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ControllerSelect : MonoBehaviour {
-
 	Toggle myToggle;
+
 	Stack controllerStack;
 
-	// Use this for initialization
 	void Start () {
-		
+		resetPlayerControllerToDefault();
+
+		initPlayers();
+		initControllerStack();
+	}
+
+	void initPlayers() {
 		GameObject players = GameObject.Find("Players");
 		int childCount = players.transform.childCount;
 
-		for(int i = GameInfo.playerCount; i < childCount ; i++)
-		{
+		for(int i = GameInfo.playerCount; i < childCount ; i++) {
 			players.transform.GetChild(i).gameObject.SetActive(false);
 		}
+	}
+
+	void initControllerStack() {
+		const int NUM_CONTROLLERS = 8;
 
 		controllerStack = new Stack();
-		//Reset Player Controls to Default
+		for(int i = NUM_CONTROLLERS; i > 0; i--) {
+			controllerStack.Push("Controller" + i);
+		}
+	}
+
+	void resetPlayerControllerToDefault() {
 		GameInfo.inputMap.Clear();
 		GameInfo.inputMap.Add("PlayerOne", "PlayerOne");
 		GameInfo.inputMap.Add("PlayerTwo", "PlayerTwo");
@@ -31,28 +44,20 @@ public class ControllerSelect : MonoBehaviour {
 		GameInfo.inputMap.Add("PlayerSix", "PlayerSix");
 		GameInfo.inputMap.Add("PlayerSeven", "PlayerSeven");
 		GameInfo.inputMap.Add("PlayerEight", "PlayerEight");
-
-	
-	for(int i = 8; i > 0; i--)
-		{
-			controllerStack.Push("Controller" + i);
-		}
 	}
 
-	public void Done()
-	{
+	public void Done() {
 		Toggle[] allChildren = GetComponentsInChildren<Toggle>(true);
-		foreach(Toggle child in allChildren)
-		if(child.GetComponent<Toggle>().isOn)
-		{
-			GameInfo.inputMap[child.name] = (string)controllerStack.Pop();
+
+		foreach(Toggle child in allChildren) {
+			if(child.GetComponent<Toggle>().isOn) {
+				GameInfo.inputMap[child.name] = (string)controllerStack.Pop();
+			}
 		}
 
 		SceneManager.LoadScene(GameInfo.playerCount);
-	
 	}
-	// Update is called once per frame
+
 	void Update () {
-		
 	}
 }
